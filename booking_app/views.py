a303bd4c2648e74cbbc5c0cedc1d59e6a61059b5
AkshayJay29 <akshayjayn29@gmail.com>
2024-11-08 06:52:44 +0000

    add views for booking, menu display, and confirmation.

--- booking_app/views.py
+++ booking_app/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, redirect
+from .models import MenuItem, Booking
+from .forms import BookingForm
+
+def index(request):
+    form = BookingForm()
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('booking_confirmation')
+    return render(request, 'booking_app/index.html', {'form': form})
+
+def menu(request):
+    items = MenuItem.objects.all()
+    return render(request, 'booking_app/menu.html', {'items': items})
+
+def booking_confirmation(request):
+    return render(request, 'booking_app/booking_confirmation.html')
+
