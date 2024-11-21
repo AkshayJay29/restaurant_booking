9780f9bdc5b0b2f27b1a236e62847ae3e1ab0212
AkshayJay29 <akshayjayn29@gmail.com>
2024-11-21 10:54:46 +0000

    add remaining configurations

--- booking_app/urls.py
+++ booking_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'booking_app'  # Namespace for the app
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('menu/', views.menu, name='menu'),
-    path('booking-confirmation/', views.booking_confirmation, name='booking_confirmation'),
+    path('confirmation/', views.booking_confirmation, name='booking_confirmation'),
 ]
--- restaurant_booking/ urls.py
+++ restaurant_booking/ urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('booking_app.urls')),
+    path('', include('booking_app.urls')),  # Correctly include the app's URL
 ]
